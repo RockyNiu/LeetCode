36b4a4b28b7cd11e8354e6a23038357cec55098c
RockyNiu <lei.niu.ny@gmail.com>
2014-03-10 15:26:46 -0400

    RecoverBinarySearchTree

---
+++ src/leetcode/java/RecoverBinarySearchTree.java
@@ -0,0 +1,33 @@
+package leetcode.java;
+
+public class RecoverBinarySearchTree {
+
+	/**
+	 * Two elements of a binary search tree (BST) are swapped by mistake.
+	 * 
+	 * Recover the tree without changing its structure.
+	 * 
+	 * Note: A solution using O(n) space is pretty straight forward. Could you
+	 * devise a constant space solution?
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution099 {
+    public void recoverTree(TreeNode root) {
+        
+    }
+}
