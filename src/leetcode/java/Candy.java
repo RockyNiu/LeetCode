7dac7b4e69a29c73b3787614a7834f4237a93467
RockyNiu <lei.niu.ny@gmail.com>
2014-04-29 17:24:26 -0400

    update Candy

--- src/leetcode/java/Candy.java
+++ src/leetcode/java/Candy.java
@@ -1,8 +1,5 @@
 package leetcode.java;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Candy {
 
 	/**
@@ -18,7 +15,7 @@ public class Candy {
 	 * What is the minimum candies you must give?
 	 */
 	public static void main(String[] args) {
-		int[] ratings = new int[] { 2,0,1};
+		int[] ratings = new int[] {1,0,2, 3};
 		Solution135 solution = new Solution135();
 		System.out.println(solution.candy(ratings));
 	}
@@ -37,51 +34,51 @@ class Solution135 {
 		
 		// index for start and end
 		int start = 0;
-		int end = 0;
+		int end = 1;
 		int flag = 0; // increased 1 or decreased -1 or equal 0
 		int highest = 0;
 		
-		int index = 1;
-		if (ratings[1] > ratings[0]){
-			flag = 1;
-		} else if (ratings[1] == ratings[0]){
+		int index = 0;
+		if (ratings[1] == ratings[0]){
 			flag = 0;
+		} else if (ratings[1] > ratings[0]){
+			flag = 1;
 		} else {
 			flag = -1;
 		}
 		
-		print(start, end, index, flag, result);
+		print(start, end, index, flag, highest, result);
 		
-		while (index < len){
-			if (ratings[index] == ratings[index-1]){
+		while (index < len - 1){
+			if (ratings[index+1] == ratings[index]){
 				flag = 0;
-				result = result + cal(end - start + 1);
+				result = result + cal(end - start);
 				start = index;
-				end = index;
-				highest = 0;
-			} else if (ratings[index] > ratings[index - 1]){
+				end = index+1;
+				highest = 1;
+			} else if (ratings[index+1] > ratings[index]){
 				if (flag == 1){
 					end++;
 				} else {
-					result = result + cal(end - start) + highest;
-					highest = end - start + 1;
-					start = index - 1;
-					end = index;
+					result = result + cal(end - start) - highest;
+					highest = 1;
+					start = index;
+					end = index+1;
 					flag = 1;
 				}
 			} else {
 				if (flag == -1){
 					end++;
 				} else {
-					result = result + cal(end - start) + highest;
-					highest = end - start + 1;
-					start = index - 1;
-					end = index;
+					result = result + cal(end - start) - highest;
+					highest = end - start;
+					start = index;
+					end = index+1;
 					flag = -1;
 
 				}
 			}
-			print(start, end, index, flag, result);
+			print(start, end, index, flag, highest, result);
 			index++;
 		}		
 		return result + cal(end - start + 1);
@@ -91,11 +88,12 @@ class Solution135 {
 		return (n + 1) * n / 2;
 	}
 
-	void print(int start, int end, int index, int flag, int result){
+	void print(int start, int end, int index, int flag, int highest, int result){
 		System.out.print("\tstart:\t" + start);
 		System.out.print("\tend:\t" + end);
 		System.out.print("\tindex:\t" + end);
 		System.out.print("\tflag:\t" + flag);
+		System.out.print("\thighest:\t" + highest);
 		System.out.print("\tresult:\t" + result + "\n");
 	}
 }
