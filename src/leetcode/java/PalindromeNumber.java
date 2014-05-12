6329c3740ff72bac50cac1d229ce7b035292a1bf
RockyNiu <lei.niu.ny@gmail.com>
2014-05-12 15:33:29 -0400

    update PalindromeNumber

--- src/leetcode/java/PalindromeNumber.java
+++ src/leetcode/java/PalindromeNumber.java
@@ -1,24 +1,52 @@
 package leetcode.java;
-/*Determine whether an integer is a palindrome. Do this without extra space.
 
-click to show spoilers.
-
-Some hints:
-Could negative integers be palindromes? (ie, -1)
-
-If you are thinking of converting the integer to string, note the restriction of using extra space.
-
-You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", 
-you know that the reversed integer might overflow. How would you handle such case?
-
-There is a more generic way of solving this problem.
-*/
+/**
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ * 
+ * click to show spoilers.
+ * 
+ * Some hints: Could negative integers be palindromes? (ie, -1)
+ * 
+ * If you are thinking of converting the integer to string, note the restriction
+ * of using extra space.
+ * 
+ * You could also try reversing an integer. However, if you have solved the
+ * problem "Reverse Integer", you know that the reversed integer might overflow.
+ * How would you handle such case?
+ * 
+ * There is a more generic way of solving this problem.
+ */
 public class PalindromeNumber {
 
 	public static void main(String[] args) {
-		int num = -2147447412;
-		Solution009 solution = new Solution009();
+		int num = 2147447412;
+		Solution009_2 solution = new Solution009_2();
 		System.out.println(solution.isPalindrome(num));
+	}
+}
+
+// Albert Chen's Solution 2014.05.12
+class Solution009_2 {
+	boolean isPalindrome(int x) {
+		int y = x;
+		int digits = 0;
+		while (y > 0) {
+			digits++;
+			y = y / 10;
+		}
+
+		y = 0;
+		for (int i = 0; i < digits / 2; i++) {
+			y *= 10;
+			y = y + x % 10;
+			x /= 10;
+		}
+
+		if (digits % 2 == 1) {
+			x /= 10;
+		}
+
+		return x == y;
 	}
 }
 
